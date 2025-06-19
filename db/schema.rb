a050c4477586c21dab91bcc6b94c4d6578f18163
jaymeweiss <jayme.weiss@zappistore.com>
2025-06-19 11:45:25 +0200

    add users table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2025_06_19_085205) do
+ActiveRecord::Schema[8.0].define(version: 2025_06_19_093947) do
   create_table "photos", force: :cascade do |t|
     t.string "title"
     t.text "description"
@@ -18,4 +18,10 @@ ActiveRecord::Schema[7.1].define(version: 2025_06_19_085205) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "users", force: :cascade do |t|
+    t.string "email"
+    t.string "password_digest"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 end

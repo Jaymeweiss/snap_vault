cabe3b0fc9011d583277a8dcd28df8de5f3b9ec4
jaymeweiss <jayme.weiss@zappistore.com>
2025-06-19 12:23:36 +0200

    link uploaded files to user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ApplicationRecord
   has_secure_password
+  has_many :uploaded_files, dependent: :destroy
 
   validates :email, presence: true, uniqueness: { case_sensitive: false }, 
             format: { with: URI::MailTo::EMAIL_REGEXP }
